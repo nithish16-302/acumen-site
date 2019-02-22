c8c50dcb88ce7a7c15ad9c0ab5594e474b764884
krushiraj.tula <krushiraj.tula@servicenow.com>
2019-02-23 02:04:33 +0530

    fixed some css
    css for location and location embed
    
    made section height 100%

--- acumen_site/acuthon/views.py
+++ acumen_site/acuthon/views.py
@@ -6,29 +6,40 @@ from .models import *
 
 
 # Create your views here.
+#All get related links are displayed in acuthon itself using modals or some divs
+#All other views are just for handling POST data
 def acuthon(request):
     return render(request, 'index.html')
 
 def register(request):
+    """
+    POST:
+    Create a User
+    After successful user creation create participant
+    Get the extra details needed for participant model
+    Commit the changes and data to DB
+    Login the user
+    """
     if request.method == 'POST':
         print(request.POST)
         return HttpResponse('POST called'+str(request.POST))
-    elif request.method == 'GET':
-        return render(request, 'registration.html')
         
 
 def login(request):
+    """
+    POST:
+    Check if the credentials are correct then login the user
+    """
     if request.method == 'POST':
         user = User.objects.create_user(**request.POST)
-    elif request.method == 'GET':
-        if request.user.is_authenticated:
-            return redirect('acuthon')
-        else:
-            return render(request, 'login.html')
         
 
 @login_required(login_url='/acuthon/login/')
 def teams(request):
+    """
+    POST:
+    Create team and add participants as CSV
+    """
     if request.method == 'POST':
         return HttpResponse('POST called')
     elif request.method == 'GET':
