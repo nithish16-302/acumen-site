06ed9e233a38c6384b7c9978ab3de95a5e569850
Nithish Muddappa <43718383+nithish16-302@users.noreply.github.com>
2019-03-11 22:41:18 +0530

    Update settings.py

--- acumen_site/settings.py
+++ acumen_site/settings.py
@@ -29,8 +29,8 @@ TEST_VERSION = False
 ALLOWED_HOSTS = ['*']
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = ''
-EMAIL_HOST_PASSWORD = ''
+EMAIL_HOST_USER = 'acumen.it.vce@gmail.com'
+EMAIL_HOST_PASSWORD = 'acumenIT@2K19'
 EMAIL_PORT = 587
 
 # Application definition
